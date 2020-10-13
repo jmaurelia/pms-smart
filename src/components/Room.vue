21b3fa961012b7ca9519d536a3f543373e635e3d
Juan Maurelia G. CELCO <juan.maurelia@arauco.com>
2020-10-13 14:52:44 -0300

    Data for room

--- src/components/Room.vue
+++ src/components/Room.vue
@@ -2,12 +2,22 @@
   <div>
     Rooms
     <h3>{{ $route.params.name }}</h3>
-    <p>{{ roomData }}</p>
+    <pre>{{ roomData }}</pre>
     <b-list-group v-if="roomData.programs" class="px-4">
-      <b-list-group-item class="d-flex" v-for="(item, index) in roomData.programs" :key="index"
-        >Programa {{ index }}
-        <b-form-checkbox switch size="lg" class="ml-auto"
-      /></b-list-group-item>
+      <b-list-group-item
+        class="d-flex align-items-center"
+        v-for="(item, index) in roomData.programs"
+        :key="index"
+      >
+        Programa {{ index }}
+        <b-button
+          @click="setProgram(index, item)"
+          class="ml-auto"
+          :class="[item ? 'btn-success' : 'btn-secondary']"
+        >
+          <b-icon icon="power" /> 
+        </b-button>
+      </b-list-group-item>
     </b-list-group>
     <span v-else>No hay</span>
   </div>
@@ -30,8 +40,22 @@ export default {
 
       roomRef.on("value", (snapshot) => {
         this.roomData = snapshot.val();
-        console.log(this.roomData);
       });
+    },
+    async setProgram(index, item) {
+      if (item === 0) {
+        var programs = this.roomData.programs;
+        var programActive = Object.keys(programs).filter(function (key) {
+          return programs[key] != 0;
+        });
+        var reference = database.ref(this.roomName + "/programs");
+
+        if (programActive.length != 0) {
+          await reference.child(String(programActive)).set(0);
+        }
+
+        await reference.child(index).set("on");
+      }
     },
   },
   created() {
