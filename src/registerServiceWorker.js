5d6b63d66f8bc32e7e15c6ec620d60578a34ad30
Juan Andres <juan.maurelia@gmail.com>
2020-12-20 19:23:07 -0300

    LATEST_VERSION = 'v1'

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -2,25 +2,25 @@ import { register } from 'register-service-worker'
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
-    ready() {
+    ready () {
       console.log('Site is ready')
     },
-    cached() {
+    cached () {
       console.log('Content has been cached for offline use.')
     },
-    updatefound() {
+    updatefound () {
       console.log('New content is downloading.')
     },
-    updated() {
+    updated () {
       console.log('New content is available; Refresh...')
       setTimeout(() => {
         window.location.reload(true)
       }, 1000)
     },
-    offline() {
+    offline () {
       console.log('No internet connection found. App is running in offline mode.')
     },
-    error(error) {
+    error (error) {
       console.error('Error during service worker registration:', error)
     },
   })
