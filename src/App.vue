49b42f027359064ce3159ce13bf2e0346b5a7e0b
Juan Andres <juan.maurelia@gmail.com>
2020-12-21 07:44:09 -0300

    Update Button

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,21 @@
 <template>
-  <div id="app"><router-view /></div>
+  <div id="app">
+    <!-- update -->
+    <b-navbar type="dark" variant="dark" v-if="updateExists">
+      <span class="text-light">Tienes una actualización</span
+      ><b-button variant="primary" size="sm" class="ml-auto" @click="refreshApp"
+        >Actualizar</b-button
+      >
+    </b-navbar>
+    <!-- ./update -->
+    <router-view />
+  </div>
 </template>
 
+<script>
+import update from "./mixins/update";
+
+export default {
+  mixins: [update],
+};
+</script>
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,34 +1,10 @@
-// service-worker.js
+// src/service-worker.js
 
-workbox.core.setCacheNameDetails({ prefix: 'd4' })
-//Change this value every time before you build
-const LATEST_VERSION = 'v2'
-self.addEventListener('activate', (event) => {
-  console.log(`%c ${LATEST_VERSION} `, 'background: #ddd; color: #0000ff')
-  if (caches) {
-    caches.keys().then((arr) => {
-      arr.forEach((key) => {
-        if (key.indexOf('d4-precache') < -1) {
-          caches.delete(key).then(() => console.log(`%c Cleared ${key}`, 'background: #333; color: #ff0000'))
-        } else {
-          caches.open(key).then((cache) => {
-            cache.match('version').then((res) => {
-              if (!res) {
-                cache.put('version', new Response(LATEST_VERSION, { status: 200, statusText: LATEST_VERSION }))
-              } else if (res.statusText !== LATEST_VERSION) {
-                caches.delete(key).then(() => console.log(`%c Cleared Cache ${LATEST_VERSION}`, 'background: #333; color: #ff0000'))
-              } else console.log(`%c Great you have the latest version ${LATEST_VERSION}`, 'background: #333; color: #00ff00')
-            })
-          })
-        }
-      })
-    })
+// If you are using 'GenerateSW' (default) for your workboxPluginMode setting this file is auto generated for you.
+// If you are using 'InjectManifest' then add this to your custom SW after your standard workbox config
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting()
   }
 })
-
-workbox.skipWaiting()
-workbox.clientsClaim()
-
-self.__precacheManifest = [].concat(self.__precacheManifest || [])
-workbox.precaching.suppressWarnings()
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
